a4cdf0e40c09d72b2fc1951b057960c4705929fb
Takeshi Ooka <74577802+t-ooka@users.noreply.github.com>
2026-06-02 23:51:36 +0900

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode*> visited;
+        ListNode* node = head;
+        
+        while (node) {
+            if (visited.find(node) != visited.end()) {
+                return true;
+            }
+            visited.insert(node);
+            node = node->next;
+        }
+
+        return false;
+    }
+};
